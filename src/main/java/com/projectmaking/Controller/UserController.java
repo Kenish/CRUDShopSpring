e8e4b5476973c540e9c4a9883fe82ad1db01cd53
vittin <97mati@wp.pl>
2016-07-06 20:06:03 +0200

    typos fixed

--- src/main/java/com/projectmaking/Controller/UserController.java
+++ src/main/java/com/projectmaking/Controller/UserController.java
@@ -14,36 +14,36 @@ public class UserController {
     private UserRepository repository;
     @Autowired
     public UserController(UserRepository repository){
-       this.repository=repository ;
+       this.repository = repository ;
     }
 
 
-    @RequestMapping(value = "users",method = RequestMethod.GET)
+    @RequestMapping(value = "users", method = RequestMethod.GET)
     public List<User> list(){
         return repository.findAll();
     }
 
-    @RequestMapping(value = "users/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "users/{id}", method = RequestMethod.GET)
     public User get(@PathVariable Long id){
        return repository.findOne(id);
     }
 
-    @RequestMapping(value = "users",method = RequestMethod.POST)
+    @RequestMapping(value = "users", method = RequestMethod.POST)
     public User add(@RequestBody User user){
         return repository.save(user);
     }
 
 
-    @RequestMapping(value = "users/{id}",method = RequestMethod.DELETE)
+    @RequestMapping(value = "users/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id){
         repository.delete(id);
     }
 
-    @RequestMapping(value = "users/{id}",method = RequestMethod.PUT)
+    @RequestMapping(value = "users/{id}", method = RequestMethod.PUT)
     public void update(@RequestBody User user, @PathVariable Long id){
-        User olduser=repository.findOne(id);
-        BeanUtils.copyProperties(user,olduser);
-        repository.saveAndFlush(olduser);
+        User oldUser = repository.findOne(id);
+        BeanUtils.copyProperties(user,oldUser);
+        repository.saveAndFlush(oldUser);
     }
 
 }
