a2c194f285c68d72360782872d935bd57f04c579
vittin <97mati@wp.pl>
2016-07-08 13:52:30 +0200

    prepare json, send, consume response

--- src/main/java/com/projectmaking/Controller/HomeController.java
+++ src/main/java/com/projectmaking/Controller/HomeController.java
@@ -3,14 +3,13 @@ package com.projectmaking.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 
 public class HomeController {
-    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String home(){
-        return "redirect:/index.html";
+        return "/registerPage.html";
     }
 
 }
