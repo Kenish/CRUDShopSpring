247ed4c8acb293a3d4cd256dbcb9df58df711b9d
kenish <przemo9712@gmail.com>
2017-02-28 09:55:38 +0100

    npm auto build

--- src/main/java/com/projectmaking/Config/DatabaseLoader.java
+++ src/main/java/com/projectmaking/Config/DatabaseLoader.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
